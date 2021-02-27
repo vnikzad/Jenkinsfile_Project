0a2e9a7842f85bfcf8381c6dc033ccb44cca5235
student <v.nikzad@gmail.com>
2021-02-27 17:10:01 +0000

    New Commit

---
+++ src/main/java/com/simplilearn/Jenkinsfile/JenkinsfileApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Jenkinsfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Jenkinsfile/JenkinsfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Jenkinsfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
